9c9a5a4b6fd4da0df63c2baffe61851881c6375e
hfccr <hfccr@outlook.com>
2024-10-13 17:28:25 +0530

    Add multi provider support

---
+++ src/components/ReclaimAssets.ts
@@ -0,0 +1,16 @@
+import { Provider } from "./InvitedLeaderTable";
+
+export const ReclaimAssets = {
+  [Provider.GITHUB]: {
+    PROVIDER_ID: "6d3f6753-7ee6-49ee-a545-62f1b1822ae5",
+  },
+  [Provider.X]: {
+    PROVIDER_ID: "475b5501-427b-47c8-b007-fb7837cd8b5c",
+  },
+  [Provider.INSTAGRAM]: {
+    PROVIDER_ID: "3ad6946f-88f4-4958-9a8e-5271a831b5b8",
+  },
+  [Provider.YOUTUBE]: {
+    PROVIDER_ID: "5a939797-afe0-4ad9-8dc4-6db967841a2c",
+  },
+};
--- src/components/ReclaimRequest.tsx
+++ src/components/ReclaimRequest.tsx
@@ -7,6 +7,7 @@ import {
 import QRCode from "react-qr-code";
 import { Alert, Box, Skeleton } from "@mui/material";
 import { getProviderName } from "./InvitedLeaderTable";
+import { ReclaimAssets } from "./ReclaimAssets";
 
 interface ReclaimRequestProps {
   provider: number;
@@ -25,10 +26,17 @@ export default function ReclaimRequest({
 
   useEffect(() => {
     async function initializeReclaim() {
-      const APP_ID = "0x62deA5dF1940A84A408Ec2446Db8824aA704c053";
+      const APP_ID = "0x5473aBf898443e1277ec079563BcA9f9aEe5d6e8";
+      // const APP_ID = "0x62deA5dF1940A84A408Ec2446Db8824aA704c053";
       const APP_SECRET =
-        "0xc813b556f829dcead74c34d7685b7168745d43fdc13d50f40aaea256a0d832ba";
-      const PROVIDER_ID = "6d3f6753-7ee6-49ee-a545-62f1b1822ae5";
+        "0x097972479cadfb156da8f0e796f8f807140444537df7eff4fc633fcebc628d78";
+      const PROVIDER_ID =
+        ReclaimAssets[provider as keyof typeof ReclaimAssets].PROVIDER_ID;
+      // const APP_SECRET =
+      //   "0xc813b556f829dcead74c34d7685b7168745d43fdc13d50f40aaea256a0d832ba";
+      // const PROVIDER_ID = "6d3f6753-7ee6-49ee-a545-62f1b1822ae5";
+
+      console.log("APp id", APP_ID, APP_SECRET, PROVIDER_ID);
 
       const reclaimProofRequest = await ReclaimProofRequest.init(
         APP_ID,
