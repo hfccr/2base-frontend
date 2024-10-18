bf71018b570a1000103c73240450740875628b89
brijeshagal <brijeshagal@gmail.com>
2024-10-19 02:02:56 +0530

    add twitter sdk

--- src/app/api/twitter/route.ts
+++ src/app/api/twitter/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import { TwitterApi } from "twitter-api-v2"; // Use only twitter-api-v2
+import { Client, auth } from "twitter-api-sdk";
 // import dotenv from "dotenv";
 
 // // Load environment variables (assuming you're using .env.local in Next.js)
@@ -7,12 +7,12 @@ import { TwitterApi } from "twitter-api-v2"; // Use only twitter-api-v2
 
 // Twitter API credentials from environment variables
 
-// const client = new TwitterApi({
-//   apiKey: process.env.TWITTER_API_KEY || "",
-//   appSecret: process.env.TWITTER_API_SECRET_KEY,
-//   accessToken: process.env.TWITTER_ACCESS_TOKEN,
-//   accessSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
-// });
+const authClient = new auth.OAuth2User({
+  client_id: process.env.CLIENT_ID as string,
+  client_secret: process.env.CLIENT_SECRET as string,
+  callback: "YOUR-CALLBACK",
+  scopes: ["tweet.read", "users.read", "offline.access"],
+});
 
 // const client = new TwitterApi(process.env.TWITTER_BEARER_TOKEN || "");
 
@@ -25,12 +25,23 @@ export async function POST(req: NextRequest) {
   console.log("hello to test");
   // if (req.method === "POST") {
 
-  const twitterClient = new TwitterApi(process.env.TWITTER_BEARER_TOKEN || "");
-  const readOnlyClient = twitterClient.readOnly;
-
   const { twitterHandle, message, transactionHash } = await req.json();
 
   try {
+    const client = new Client(authClient);
+    const postTweet = await client.tweets.createTweet({
+      // The text of the Tweet
+      text: "Are you excited for the weekend?",
+
+      // Options for a Tweet with a poll
+      poll: {
+        options: ["Yes", "Maybe", "No"],
+        duration_minutes: 120,
+      },
+    });
+    console.dir(postTweet, {
+      depth: null,
+    });
     // If there's a transactionHash, handle it with Web3 logic
     // if (transactionHash) {
     // Use publicClient to get transaction details
@@ -40,10 +51,8 @@ export async function POST(req: NextRequest) {
     //   return res.status(404).json({ error: "Transaction not found." });
     // }
 
-    const web3Message = "hello";
 
     // Send Twitter notification based on Web3 event using twitter-api-v2
-    await twitterClient.v2.tweet(`@${twitterHandle} ${web3Message}`);
 
     return NextResponse.json({
       status: 200,
