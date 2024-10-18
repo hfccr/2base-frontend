25f813e44bdb98509693540a9793b93eda096ba9
hfccr <hfccr@outlook.com>
2024-10-18 22:21:44 +0530

    Switch hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,4 @@
-import { Box, Container, Stack } from "@mui/material";
+import { Avatar, Box, Chip, Container, Stack } from "@mui/material";
 import Typography from "@mui/material/Typography";
 import Grid from "./Grid";
 import Image from "next/image";
@@ -11,7 +11,7 @@ export default function Hero() {
     <Container sx={{ mt: 2 }}>
       <Stack
         direction="row"
-        sx={{ minHeight: "80vh", overflow: "hidden" }}
+        // sx={{ minHeight: "80vh", overflow: "hidden" }}
         justifyContent="center"
         alignItems="center"
       >
@@ -19,10 +19,10 @@ export default function Hero() {
           sx={{
             display: "flex",
             flexDirection: "column",
-            alignItems: "flex-start",
+            alignItems: "center",
             width: "200",
             fontFamily: "Paytone One",
-            zIndex: 1000,
+            // zIndex: 1000,
           }}
         >
           <Image
@@ -33,16 +33,51 @@ export default function Hero() {
             priority
           />
           <Typography variant="h1" sx={{ fontFamily: "Paytone One" }}>
-            Bring
+            Bring Web2 On Base
           </Typography>
-          <Typography variant="h1" sx={{ fontFamily: "Paytone One" }}>
-            Web2
-          </Typography>
-          <Typography variant="h1" sx={{ fontFamily: "Paytone One" }}>
-            On Base
-          </Typography>
+          <Stack direction="row" spacing={2} sx={{ mt: 4 }}>
+            <Chip
+              label="Base"
+              sx={{ fontSize: "large" }}
+              avatar={
+                <Avatar
+                  alt="Base"
+                  src="https://www.base.org/_next/static/media/logo.f6fdedfc.svg"
+                />
+              }
+            />
+            <Chip
+              label="OnchainKit"
+              sx={{ fontSize: "large" }}
+              avatar={
+                <Avatar
+                  alt="OnchainKit"
+                  src="https://onchainkit.xyz/favicon/48x48.png?v4-19-24"
+                />
+              }
+            />
+            <Chip
+              label="Basenames"
+              sx={{ fontSize: "large" }}
+              avatar={
+                <Avatar
+                  alt="Basenames"
+                  src="https://www.base.org/_next/static/media/4.40019cd0.svg"
+                />
+              }
+            />
+            <Chip
+              label="zkTLS"
+              sx={{ fontSize: "large" }}
+              avatar={
+                <Avatar
+                  alt="Reclaim Protocol"
+                  src="https://avatars.githubusercontent.com/u/130321117?s=200&v=4"
+                />
+              }
+            />
+          </Stack>
         </Box>
-        <Grid />
       </Stack>
     </Container>
   );
