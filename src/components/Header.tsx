8f37b0dce42d809a8809e16e37beb219146e18f4
hfccr <hfccr@outlook.com>
2024-10-19 15:04:54 +0530

    Keep logo visible in md

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -90,7 +90,7 @@ export default function Header() {
                         display: {
                           xs: "none",
                           sm: "none",
-                          md: "none",
+                          md: "block",
                           lg: "block",
                         },
                         paddingTop: 2,
