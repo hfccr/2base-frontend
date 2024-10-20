22dae9d9688a5dbf9e4f1c90136325b9664d6347
hfccr <hfccr@outlook.com>
2024-10-21 02:27:52 +0530

    Add check for name

--- src/components/Connect.tsx
+++ src/components/Connect.tsx
@@ -22,7 +22,7 @@ export default function Connect() {
     <Wallet>
       <ConnectWallet>
         {isConnected && <Avatar className="h-6 w-6" />}
-        <Name />
+        {isConnected && <Name />}
       </ConnectWallet>
       <WalletDropdown>
         <Identity className="px-4 pt-3 pb-2" hasCopyAddressOnClick>
